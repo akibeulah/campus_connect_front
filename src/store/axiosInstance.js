7bf163a00919ded3b6ea38891ef0fd546f42b9c2
Akindele Beulah <akibeulah@gmail.com>
2022-06-02 19:45:41 +0100

    Corrected Production api url condition statement

--- src/store/axiosInstance.js
+++ src/store/axiosInstance.js
@@ -11,7 +11,7 @@ const getHeaders = () => {
 }
 
 const axiosInstance = axios.create({
-    baseURL: (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? process.env.REACT_APP_DEV_API_URL : process.env.REACT_APP_PROD_API_URL,
+    baseURL: ((!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? process.env.REACT_APP_DEV_API_URL : process.env.REACT_APP_PROD_API_URL),
     headers: getHeaders()
 })
 
